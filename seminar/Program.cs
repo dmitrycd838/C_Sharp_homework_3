292e7ea375c4c5ce4d11d14c76a78837b8dc2ab6
Churilov.Dmitry <churilov.dmitrycd@mail.ru>
2023-11-29 18:17:11 +0300

    Код из лекции

---
+++ seminar/Program.cs
@@ -0,0 +1,58 @@
+﻿int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// // Создаём функцию, которая возвращает массив, внутрь принимает следующие значения: 
+// 1. размер массива
+// 2. Граница генерации - от какого и до какого значения мы генерируем массив 
+
+int[] GenerateArray(int size, int leftRange, int rightRange)
+{
+    // Внутри функции объявляем массив размером size. 
+    int[] tempArray = new int[size];
+    // Задаём объект класса Random для генерации случайных чисел. 
+    Random rand = new Random();
+    // Используем цикл for
+    for (int i = 0; i < size; i++)
+    {
+        tempArray[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    // Вернуть значение массива
+    return tempArray;
+}
+
+// Выводим вывод массива в отдельную функцию
+void PrintArray(int[] array)
+{
+    System.Console.Write("[" + string.Join(", ", array) + "]");
+}
+
+// Объявим функцию для поиска числа в массиве
+
+void FindNumber(int[] array, int N)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] == N)
+        {
+            System.Console.Write("Да");
+            return;
+        }
+    }
+
+    System.Console.Write("Нет");
+}
+
+
+int size = ReadInt("Введите размер массива: ");
+// Объявим массив, который равен работе метода GenerateArray, внутрь метода мы отправим
+// 1. размер генерируемого массива
+// 2. левую границу генерации.
+// 3. правую границу генерации. 
+int[] myArray = GenerateArray(size, -10, 10); 
+PrintArray(myArray);
+
+int number = ReadInt("Введите искомое число: ");
+FindNumber(myArray, number);
